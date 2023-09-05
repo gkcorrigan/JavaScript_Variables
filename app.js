4fe0b1d0e2e5dd248b16c2c7bec241e07a275e85
Graeme Corrigan <corrigangk@gmail.com>
2023-09-05 14:16:51 -0400

    introjsfinished

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
+
 
 // Exercise 2
+const firstName = "Graeme";
+lastName = "Corrigan";
+age = 28;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Naming of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 772;
+let bookmark = 456;
+let hasRead = true;
